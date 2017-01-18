12212f9906a4eb9f67f84b233c3626f95060e573
Pulecz <pulec.mcm@gmail.com>
2017-01-18 12:02:50 +0100

    Nicer print states, fixed some logic in eveluate_score def, hopefully it will not print "Your performance needs no comments." so often. And gave a hint why bulls always run away if player don't get the whole bull_population

--- bullsNcows.py
+++ bullsNcows.py
@@ -13,12 +13,12 @@ def create_number(bulls):
 
 
 def print_intro(bulls):
-    print("""Welcome, let's play a game where you have to guess a number. \
-People call it bulls and cows. You will have a chance to win {0} bulls, \
-which you will get for each correctly guessed number. For each incorrect \
-guess you will get a cow. And you will sure need those to get sucessfull \
-herd.""".format(bulls)
-)
+    print("Welcome, let's play a game where you have to guess a number.\n"
+          "People call it bulls and cows.\n"
+          "You will have a chance to win {0} bulls, which you will get for "
+          "each correctly guessed number.\n"
+          "For each incorrect guess you will get a cow. And you will sure need "
+          "those to get sucessfull.\n".format(bulls))
 
 
 def ask_user(bulls):
@@ -51,42 +51,47 @@ def count_bulls_and_cows(guess, target):
 
 
 def evaluate_score(loops, total_cows):
-    if loops == 0 and total_cows == 0:
-        print("Congratulations. You guessed the number at first try!")
-        print("Good luck maintaining your herd with no cows...")
-    elif loops > 0 and loops <= 2 and total_cows < 3:
+    """Try to rate players performance based on loops and total_cows"""
+    if loops == 1 and total_cows == 0:
+        print("Congratulations. You guessed the number at first try!\n"
+              "Good luck maintaining your herd with no cows...")
+    elif loops > 1 and loops <= 4 and total_cows <= 3:
         print("Very good, you could use more cows though.")
-    elif loops > 3 and loops <= 7 and total_cows <= 16:
-        print("Impressive, just enough cows in less then five guesses.")
-    elif loops > 3 and loops <= 7:
+    elif loops > 1 and loops <= 4 and total_cows >= 4 and total_cows <15:
+        print("Very good, ideal start for a new herd.")
+    elif loops >= 5 and loops <= 7 and total_cows <= 16:
+        print("Impressive, just enough cows in less then seven guesses.")
+    elif loops >= 3 and loops <= 7:
         print("Impressive. Quite a herd.")
-    elif loops > 8:
+    elif loops >= 8 and loops <= 15:
         print("Nice guess. You were lucky.")
-    elif loops > 16:
-        print("You could do better.")
+    elif loops >= 16 and loops <= 31:
+        print("You could do better though.")
     else:
         print("Your performance needs no comments.")
 
 
 def the_game(bull_population):
+    """The main game logic"""
     loops = 1
     total_cows = 0
     print_intro(bull_population)
     the_number = create_number(bull_population)
+    print(the_number)
     while True:
         player_guess = ask_user(bull_population)
         bulls, cows = count_bulls_and_cows(player_guess, the_number)
         if bulls == bull_population:
             total_cows += cows
-            print("Correct, you got all the {0} bulls! It took you {1} guesses.\
- You also have {2} cows."
-                  .format(bull_population, loops, total_cows))
+            print("Correct, you got all the {0} bulls! It took you {1} "
+                  "guesses. Finally the bulls stay with the {2} cows "
+                  "you collected.".format(bull_population, loops, total_cows))
             evaluate_score(loops, total_cows)
             exit(0)
         else:
             loops += 1
             total_cows += cows
-            print("You have {0} bulls and {1} cows. Still need to get {2} bulls."
-                  .format(bulls, cows, bull_population - bulls))
+            print("You got {0} bulls and {1} cows. Bulls ran away..."
+                  .format(bulls, cows))
 
 the_game(bull_population)
